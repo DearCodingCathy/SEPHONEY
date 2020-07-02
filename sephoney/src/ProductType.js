e05790c5e94fa831c9e02c94fe710e4029c69383
Catherine Balogun <catherinebalogun@Catherines-MBP.lan>
2020-07-02 15:22:18 -0400

    before deploy

--- sephoney/src/ProductType.js
+++ sephoney/src/ProductType.js
@@ -1,7 +1,11 @@
 import React from 'react'
 // import {Link} from 'react-router-dom'
 
-export default function ProductType() {
+export default function ProductType(props) {
+  const uniqueTypes = new Set(props.products.map(i => {
+    return i.product_type
+  }))
+  console.log(uniqueTypes);
   return (
     <div>
       <h1> This is where the product types will be! </h1>
