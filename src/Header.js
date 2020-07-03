a4ba05f7a5427f3f4178c1dcb559e6f97183a774
Catherine Balogun <catherinebalogun@Catherines-MBP.lan>
2020-07-02 21:08:54 -0400

    new home img

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Search from './Search'
 
 export default function Header() {
   return (
     <div className='d-flex justify-content-around'>
-      {/* <Search /> */}
       <Link className='text-dark font-weight-lighter text-decoration-none text-center' to='/'><h1>SEPHONEY</h1> </Link>
 
       <Link className='text-dark text-decoration-none' to='/register'><h5><small>Sign In/ Register</small></h5></Link>
--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <div className='home-container'>
-      <img className='homeImg' src='https://i.imgur.com/EZbXlUc.jpg' alt='Makeup' />
+      <img className='homeImg' src='https://i.imgur.com/YspOJf4.jpg' alt='Responsive image' />
       <Link to='/allproducts'><button className='btn btn-light btn-outline-secondary btn-lg'>Shop Now</button></Link>
 
     </div>
